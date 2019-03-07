895c92640e993c3bca618f05683de0b7ac2db0aa
shikhagupta9 <shikhag1299@gmail.com>
2019-03-07 22:31:43 +0530

    updatenew

--- new.c
+++ new.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 #define rest 0
 #define cafe 1
 #define pub 2
+
 #define asgard 0
 #define narnia 1
 #define wallstreet 2
@@ -70,16 +72,108 @@ typedef struct category
     outlet *type[4];
 }category;
 
+/*------------------------------------------------------*/
+agent *free_agents[5];
+agent *alloc;
+/*------------------------------------------------------*/
 void initialize(category cat[]);
 void addoutlet(category cat[]);
+void address_name(char c[],int address_code);
+void traverse_outlet(outlet *o,int address);
+void search_category(category cat[],user *u);
+/*------------------------------------------------------*/
+
+
+
+int main()
+{
+    category cat[3];
+    initialize(cat);
+    
+    return 0;
+}
+
+/*------------------------------------------------------*/
+
+void initialize(category cat[])
+{
+    int i=0,j=0;
+    for(i=0;i<3;i++)
+    {
+        for (j=0; j<4; j++)
+        {
+            cat[i].type[i]=NULL;
+        }
+        
+       
+    }
+}
+
+/*------------------------------------------------------*/
+
+int distance(int first,int second)
+{
+    int d;
+    d=(X[first]-X[second])*(X[first]-X[second])+(Y[first]-Y[second])*(Y[first]-Y[second]);
+    return d;
+}
+
+/*------------------------------------------------------*/
+
+void address_name(char c[],int address_code)
+{
+    char *a;
+    switch (address_code)
+    {
+        case 0:
+            a="asgard";
+            strcpy(c, a);
+            break;
+        case 1:
+            a="narnia";
+            strcpy(c, a);
+            break;
+        case 2:
+            a="wallstreet";
+            strcpy(c, a);
+            break;
+        case 3:
+            a="diagonalley";
+            strcpy(c, a);
+            break;
+        case 4:
+            a="pinkcity";
+            strcpy(c, a);
+            break;
+        default:
+            break;
+    }
+}
+
+/*------------------------------------------------------*/
 
 void traverse_outlet(outlet *o,int address)
 {
-    while (o!=NULL)
+    int near[3]={1000,1000,1000};
+    int d,i,j,flag;
+    char ad[20];
+    if (address!=-1)
+    {
+        while (o!=NULL)
+        {
+            address_name(ad,o->add_code);
+            printf("-----------------\nfood outlet name-%s\nNumber of seats-%daddress-%s\n------------\n",o->name,o->seats,ad);
+        }
+    }else
     {
         
+            
     }
+    
 }
+
+/*------------------------------------------------------*/
+
 void search_category(category cat[],user *u)
 {
     int t,index1,index2,i,address=-1;
@@ -97,19 +191,19 @@ void search_category(category cat[],user *u)
     }
     if(index1<3)
     {
-            select=cat[index1];
-            if(t<=4)
-            {   ptr=select.type[t-1];
+        select=cat[index1];
+        if(t<=4)
+        {   ptr=select.type[t-1];
+            traverse_outlet(ptr,address);
+        }
+        else
+        {
+            for(i=0;i<4;i++)
+            {
+                ptr=select.type[i];
                 traverse_outlet(ptr,address);
             }
-            else
-            {
-                for(i=0;i<4;i++)
-                {
-                    ptr=select.type[i];
-                    traverse_outlet(ptr,address);
-                }
-            }
+        }
     }
     else
     {
@@ -131,38 +225,9 @@ void search_category(category cat[],user *u)
         }
     }
     
-   
-}
-
-
-int main()
-{
-    category cat[3];
-    initialize(cat);
     
-    return 0;
 }
 
-void initialize(category cat[])
-{
-    int i=0,j=0;
-    for(i=0;i<3;i++)
-    {
-        for (j=0; j<4; j++)
-        {
-            cat[i].type[i]=NULL;
-        }
-        
-       
-    }
-}
-
-int distance(int first,int second)
-{
-    int d;
-    d=(X[first]-X[second])*(X[first]-X[second])+(Y[first]-Y[second])*(Y[first]-Y[second]);
-    return d;
-}
 
 
 
